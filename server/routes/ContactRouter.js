a4012557cf7dd46e2f12516697939cac06a670d7
Arpan <145697806+arpanmehta05@users.noreply.github.com>
2025-03-07 21:50:00 +0530

    Delete server directory

--- server/routes/ContactRouter.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { contactUs } = require("../controllers/ConatactUs");
-
-router.post("/contactUs", contactUs);
-
-module.exports = router;
