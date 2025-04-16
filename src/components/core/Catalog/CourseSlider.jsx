8a832dca1d8a2cbdc22c675e61b5ec0d384114ab
Arpan <145697806+arpanmehta05@users.noreply.github.com>
2025-04-16 19:51:08 +0530

    feat: add Swiper for course slider and implement Course_Card with average rating display

--- src/components/core/Catalog/CourseSlider.jsx
+++ src/components/core/Catalog/CourseSlider.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/pagination";
+import { FreeMode, Pagination } from "swiper/modules";
+import Course_Card from "./Course_Card";
 
-export default function CourseSlider() {
+export default function CourseSlider({ course }) {
   return (
-    <div>
-      
-    </div>
-  )
+    <>
+      {course?.length ? (
+        <Swiper
+          slidesPerView={1}
+          spaceBetween={25}
+          loop={true}
+          modules={[FreeMode, Pagination]}
+          breakpoints={{
+            1024: {
+              slidesPerView: 3,
+            },
+          }}
+          className="max-h-[30rem]"
+        >
+          {course.map((courses, i) => {
+            <SwiperSlide key={i}>
+              <Course_Card course={courses} Height={"h-[250px]"} />
+            </SwiperSlide>;
+          })}
+        </Swiper>
+      ) : (
+        <p className="text-xl text-[#f1f2ff]">No Course Found</p>
+      )}
+    </>
+  );
 }
--- src/components/core/Catalog/Course_Card.jsx
+++ src/components/core/Catalog/Course_Card.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import GetAvgRating from "../../../utils/avgRating";
+import { Link } from "react-router-dom";
+import RatingStars from "../../common/RatingStars";
 
-export default function Course_Card() {
+export default function Course_Card({ course, Height }) {
+  const [avgReviewCount, setAvgReviewCount] = useState(0);
+
+  useEffect(() => {
+    const count = GetAvgRating(course.ratingAndReviews);
+    setAvgReviewCount(count);
+  }, [course]);
   return (
-    <div>
-      
-    </div>
-  )
+    <>
+      <Link to={`/courses/${course._id}`}>
+        <div>
+          <div className="rounded-lg">
+            <img
+              src={course?.thumbnail}
+              alt="course thumbnai"
+              className={`${Height} w-full rounded-xl object-cover`}
+            />
+          </div>
+          <div className="flex flex-col gap-2 px-1 py-3">
+            <p className="text-xl text-[#f1f2ff]">{course?.courseName}</p>
+            <p className="text-sm text-[#C5C7D4]">
+              {course?.instructor?.firstName} {course?.instructor?.lastName}
+            </p>
+            <div className="flex items-center gap-2">
+              <span className="text-[#FFF970]">{avgReviewCount || 0}</span>
+              <RatingStars ReviewCount={avgReviewCount} />
+              <span className="text-[#6E727F]">
+                {course?.ratingAndReviews?.length}Ratings{" "}
+              </span>
+              <p className="text-xl text-[#f1f2ff]">Rs. {course?.price}</p>
+            </div>
+          </div>
+        </div>
+      </Link>
+    </>
+  );
 }
